7308fe689650f5d363cbb96de5748addf52c98a5
Adrian Kiva <akiva@mta.ca>
2021-12-02 13:16:48 -0500

    semi refactor for module

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,16 @@
 const request = require('request');
 const arg = process.argv.slice(2);
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${arg[0]}`;
 
 
-const breedFetcher = () => {
+const fetchBreedDescription = function(breedName, callback(err, desc)) {
+if (error) {
+  callback(error)
+} else {callback()}
 
-request(url, (err, response, body) => {
-    if (err) {console.log('Error: ', err)}
-    if (body === '[]') {console.log('Breed not found.')}
-    else {
-      console.log(body);
-      const data = JSON.parse(body);
-      console.log(typeof data);
-      console.log(data[0].description);
-    }
-  })
+
 
 }
 
-breedFetcher();
 
-module.exports = { breedFeatcher };
+module.exports = { fetchBreedDescription };
--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const breedFetcher = require('./breedFetcher');
+const {fetchBreedDescription} = require('./breedFetcher');
+
+
+
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedname}`;
+
+fetchBreedDescription(breedName, (error, desc) => {
+
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+
+})
+
+request(url, (err, response, body) => {
+  if (err) {console.log('Error: ', err)}
+  if (body === '[]') {console.log('Breed not found.')}
+  else {
+    console.log(body);
+    const data = JSON.parse(body);
+    console.log(typeof data);
+    console.log(data[0].description);
+  }
+})
