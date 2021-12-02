340e3fc9a105e1144d231d0cbbef24addab38ed7
Adrian Kiva <akiva@mta.ca>
2021-12-02 10:09:52 -0500

    add catapi api to talk to cats with breed request

---
+++ breedFetcher.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const arg = process.argv.slice(2);
+
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${arg[0]}`;
+
+
+const breedFetcher = () => {
+
+request(url, (err, response, body) => {
+    if (err) {console.log('Error: ', err)}
+    if (body === '[]') {console.log('Breed not found.')}
+    else {
+      console.log(body);
+      const data = JSON.parse(body);
+      console.log(typeof data);
+      console.log(data[0].description);
+    }
+  })
+
+}
+
+breedFetcher();
